9cbac403ab404b27fbe982edd9e64d437be6d68f
RaylLuxraCarthe <RaphaelRc123@gmail.com>
2025-08-10 02:56:41 +0800

    Main Celestial Queen

--- scripts/main.js
+++ scripts/main.js
@@ -107,10 +107,12 @@
     }
   }
 
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js')
-      .then(reg => console.log('ServiceWorker registered:', reg.scope))
-      .catch(err => console.error('ServiceWorker registration failed:', err));
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(reg => {
+      console.log('Service Worker registered', reg);
+    }).catch(err => {
+      console.error('Service Worker registration failed:', err);
+    });
   });
 }
