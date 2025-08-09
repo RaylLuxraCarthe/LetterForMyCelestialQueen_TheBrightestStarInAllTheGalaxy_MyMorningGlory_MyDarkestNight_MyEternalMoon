3e1d700d62b439aa8dfdc9c898ad6645e28678a1
RaylLuxraCarthe <RaphaelRc123@gmail.com>
2025-08-10 02:20:08 +0800

    Scripts of The Celestial Queen

--- scripts/main.js
+++ scripts/main.js
@@ -106,3 +106,11 @@
       container.appendChild(heart);
     }
   }
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js')
+      .then(reg => console.log('ServiceWorker registered:', reg.scope))
+      .catch(err => console.error('ServiceWorker registration failed:', err));
+  });
+}
